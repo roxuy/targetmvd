de0d1ee9a08f53a8dbfe0fe6ede22b08200795ff
Rocio Godoy <rgodoylarrosa@gmail.com>
2023-01-23 23:48:05 -0300

    Add route for index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       }
 
       get '/topics', to: 'topics#index'
-      resources :targets, only: %i[create]
+      resources :targets, only: %i[index create]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
